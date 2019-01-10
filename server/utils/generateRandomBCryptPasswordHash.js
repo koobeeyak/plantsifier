b6fa1ffae7d1cbe1bafb9b46fc11084978e5641b
tucobenedicto <phkubiak@gmail.com>
2019-01-10 16:52:40 -0500

    comments in bcrypto utility

--- server/utils/generateRandomBCryptPasswordHash.js
+++ server/utils/generateRandomBCryptPasswordHash.js
@@ -1,6 +1,7 @@
 const generateRandomBCryptPasswordHash = () => {
-  // generate random password hashes, for seed data
-  // follow BCrypt hashing practices
+  // generate random password hashes
+  // we will integrate BCrypt package into our app and follow their hashing practices
+  // so far, this is just for sample seed data
   // https://github.com/kelektiv/node.bcrypt.js
   const hashCharSet = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$';
   const hashLength = 60;
